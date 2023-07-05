7306848a072d157fa3d26738a8aa1941eda8904b
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-07-05 11:53:02 +0700

    Add project files.

---
+++ final project/final project.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class bidangDatar {
+private:
+	int x,y;
+public:
+	bidangDatar() {
+		x = 0;
+		y = 0;
+	}
+
+	virtual void input() {};
+	virtual float Luas(int a) { return 0; }
+	virtual float Keliling(int a) { return 0; }
+	virtual void cekUkuran() { return ; }
+	void setX(int a) {
+		this->x = a;
+	}
+	int getX(){
+		return x;
+
+	}
+};
+
+class Lingkaran:public bidangDatar{
+public:
+
+	string getX() {
+		return 0;
+	}
+	float jarijari(){
+		cout << " jarijari Lingkaran" << endl;
+	}
+
+	float Luas() {
+		cout << "Luas PersegiPanjang" << endl;
+	}
+
+	float keliling() {
+		cout << "keliling PersegiPanjang" << endl;
+	}
+};
+
+
+class Persegipanjang:public bidangDatar {
+public:
+	string getY() {
+		return 0;
+	}
+	float Luas() {
+		cout << "Luas PersegiPanjang" << endl;
+	}
+
+	float keliling() {
+		cout << "keliling PersegiPanjang" << endl;
+	}
+
+	float panjang() {
+		cout << "panjang PersegiPanjang" << endl;
+	}
+};
+
+
+int main() {
+	bidangDatar* bentuk;
+	Lingkaran a;
+	Persegipanjang b;
+
+	bentuk = &a;
+	bentuk->Luas();
+	bentuk = &b;
+	bentuk->Luas();
+
+
+	bentuk = &a;
+	bentuk->Keliling();
+	bentuk = &b;
+	bentuk->Keliling();
+
+	return 0;
+}
+
+
